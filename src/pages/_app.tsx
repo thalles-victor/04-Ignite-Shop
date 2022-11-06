110686962a80b2eae82edbd52daa384c11892c81
thalles-victor <thalles.victor2012@gmail.com>
2022-11-06 15:25:36 -0300

    :bug: fix: Colocando  o globalStyle de fora do component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
   return <Component {...pageProps} />
 }
